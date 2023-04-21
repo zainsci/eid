13041e43904014459a5b61447d80bfc728686877
zainsci <60652827+zainsci@users.noreply.github.com>
2023-04-21 14:35:25 +0300

    Eid Mubarak 2023

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,148 +1,209 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { motion } from "framer-motion"
 
+import Lamp01Svg from "components/svgs/2023/lamp01.svg"
+import Lamp02Svg from "components/svgs/2023/lamp02.svg"
+import StarSvg from "components/svgs/2023/star.svg"
+import CrescentSvg from "components/svgs/2023/crescent.svg"
+import EidUlFitrMubarak from "components/svgs/2023/eid-ul-fitr-mubarak.svg"
+
+import Star01Svg from "components/svgs/2022/star01.svg"
 import Layout from "components/layout"
-import EidText from "components/svgs/eid.svg"
-import MubarakText from "components/svgs/mubarak.svg"
-import StarIcon from "components/svgs/star.svg"
-import MoonIcon from "components/svgs/moon.svg"
-import StarShape01 from "components/svgs/star01.svg"
-import StarShape02 from "components/svgs/star02.svg"
-import MadeBy from "components/svgs/made-by.svg"
 
-export default function Eid2022() {
-  return (
-    <Layout title="Eid Mubarak - 1443/2022">
-      <div className="min-w-screen min-h-screen p-8 flex bg-purple-500">
-        <div className="relative border-4 w-full flex justify-center items-center overflow-hidden bg-gradient-radial from-purple-400 to-purple-600">
-          <div className="w-full h-full text-5xl md:text-7xl font-display z-10 flex flex-col justify-center items-center">
-            <div className="flex flex-col md:flex-row items-center gap-8">
-              <div className="relative w-44 md:w-60 h-auto flex justify-center items-start">
-                <div className="text-yellow">
-                  <Moon />
-                </div>
-                <div className="absolute md:relative top-0 right-0 text-teal">
-                  <Star />
-                </div>
-              </div>
-
-              <div className="w-60 flex flex-col justify-center items-center md:items-end gap-4">
-                <div className="w-32 md:w-44">
-                  <EidText />
-                </div>
-                <div className="w-44 md:w-60">
-                  <MubarakText />
-                </div>
-              </div>
-            </div>
-            <h1 className="text-2xl mt-12">Eid Ul Fitr Mubarak</h1>
-
-            <a
-              href="https://zainsci.dev"
-              rel="noopener noreferrer"
-              target="_blank"
-              className="text-sm text-white absolute bottom-6"
-            >
-              <MadeBy />
-            </a>
-          </div>
-
-          <Star01 />
-          <Star02 />
-        </div>
-      </div>
-    </Layout>
-  )
+const Eid = () => {
+	return (
+		<Layout title="Eid Mubarak - 2023">
+			<div className="w-screen h-screen flex flex-col justify-center items-center bg-gray-200">
+				<div className="w-[30rem] h-[30rem] relative bg-gradient-to-tr from-slate-100 to-slate-200 flex flex-col justify-center items-center overflow-hidden text-gray-300 shadow-2xl shadow-teal-700 dark:from-slate-900 dark:to-slate-800">
+					<h1 className="absolute text-blue-900 dark:text-blue-500 flex z-30 bottom-16 right-14">
+						<EidUlFitrMubarak />
+					</h1>
+					<Lamp01 />
+					<Lamp02 />
+					<Crescent />
+					<Star />
+					<Star01 />
+					<ThemeToggle />
+				</div>
+			</div>
+		</Layout>
+	)
 }
 
-export function Star() {
-  return (
-    <motion.div
-      animate={{
-        rotateZ: ["10deg", "10deg", "-10deg", "-10deg", "10deg"],
-      }}
-      transition={{
-        duration: 3,
-        repeat: Infinity,
-        ease: "linear",
-      }}
-      className="w-10 h-10 md:w-14 md:h-14"
-    >
-      <StarIcon />
-    </motion.div>
-  )
+export default Eid
+
+function ThemeToggle() {
+	const [isDark, setIsDark] = useState(false)
+
+	useEffect(() => {
+		if (isDark) {
+			document.documentElement.className = ""
+		} else {
+			document.documentElement.className = "dark"
+		}
+	}, [isDark])
+
+	function handleThemeChange() {
+		if (isDark) {
+			setIsDark(false)
+		} else {
+			setIsDark(true)
+		}
+	}
+
+	return (
+		<div className="absolute top-6 right-6">
+			<button
+				onClick={handleThemeChange}
+				className="w-8 h-8 text-gray-500 dark:text-gray-100 bg-gray-300 dark:bg-gray-700 rounded-full flex justify-center items-center"
+			>
+				{isDark ? (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						className="feather feather-sun"
+					>
+						<circle cx="12" cy="12" r="5"></circle>
+						<line x1="12" y1="1" x2="12" y2="3"></line>
+						<line x1="12" y1="21" x2="12" y2="23"></line>
+						<line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+						<line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+						<line x1="1" y1="12" x2="3" y2="12"></line>
+						<line x1="21" y1="12" x2="23" y2="12"></line>
+						<line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+						<line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+					</svg>
+				) : (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						className="feather feather-moon"
+					>
+						<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+					</svg>
+				)}
+			</button>
+		</div>
+	)
 }
 
-export function Moon() {
-  return (
-    <motion.div
-      animate={{
-        rotateZ: ["10deg", "10deg", "-10deg", "-10deg", "10deg"],
-      }}
-      transition={{
-        duration: 3,
-        repeat: Infinity,
-        ease: "linear",
-      }}
-      className="w-44 h-44 md:w-56 md:h-56"
-    >
-      <MoonIcon />
-    </motion.div>
-  )
+function Lamp01() {
+	return (
+		<motion.div
+			animate={{
+				rotateZ: ["10deg", "-10deg", "10deg"],
+				transformOrigin: "center top",
+			}}
+			transition={{
+				duration: 6,
+				repeat: Infinity,
+				ease: "easeInOut",
+				bounce: 0,
+			}}
+			className="absolute top-0 left-28 text-green-600"
+		>
+			<Lamp01Svg />
+		</motion.div>
+	)
+}
+
+function Lamp02() {
+	return (
+		<motion.div
+			animate={{
+				rotateZ: ["8deg", "-8deg", "8deg"],
+				transformOrigin: "center top",
+			}}
+			transition={{
+				duration: 10,
+				repeat: Infinity,
+				ease: "easeInOut",
+				bounce: 0,
+			}}
+			className="absolute top-0 left-12 text-green-600"
+		>
+			<Lamp02Svg />
+		</motion.div>
+	)
+}
+
+function Crescent() {
+	return (
+		<motion.div
+			animate={{
+				rotateZ: ["10deg", "-10deg", "10deg"],
+				transformOrigin: "center top",
+			}}
+			transition={{
+				duration: 6,
+				repeat: Infinity,
+				ease: "easeInOut",
+				bounce: 0,
+			}}
+			className="absolute top-0 left-40 text-yellow-500"
+		>
+			<CrescentSvg />
+		</motion.div>
+	)
+}
+
+function Star() {
+	return (
+		<motion.div
+			animate={{
+				rotateZ: ["-8deg", "8deg", "-8deg"],
+				transformOrigin: "center top",
+			}}
+			transition={{
+				duration: 6,
+				repeat: Infinity,
+				ease: "easeInOut",
+				bounce: 0,
+			}}
+			className="absolute top-0 left-52 text-yellow-500"
+		>
+			<StarSvg />
+		</motion.div>
+	)
 }
 
 export function Star01() {
-  return (
-    <motion.div
-      className="absolute top-0 right-0 text-purple-600 md:text-teal opacity-60 flex justify-center items-center w-full h-full"
-      style={{
-        minWidth: "30rem",
-        minHeight: "30rem",
-        maxWidth: "60vw",
-        maxHeight: "60vw",
-      }}
-      initial={{
-        translateX: "50%",
-        translateY: "-50%",
-      }}
-      animate={{
-        rotateZ: "360deg",
-      }}
-      transition={{
-        duration: 40,
-        repeat: Infinity,
-        ease: "linear",
-      }}
-    >
-      <StarShape01 />
-    </motion.div>
-  )
-}
-
-export function Star02() {
-  return (
-    <motion.div
-      className="absolute bottom-0 left-0 opacity-60 flex justify-center items-center w-full h-full text-purple-400 md:text-yellow"
-      style={{
-        minWidth: "20rem",
-        minHeight: "20rem",
-        maxWidth: "40vw",
-        maxHeight: "40vw",
-      }}
-      initial={{
-        translateX: "-50%",
-        translateY: "50%",
-      }}
-      animate={{
-        rotateZ: "360deg",
-      }}
-      transition={{
-        duration: 20,
-        repeat: Infinity,
-        ease: "linear",
-      }}
-    >
-      <StarShape02 />
-    </motion.div>
-  )
+	return (
+		<motion.div
+			className="absolute bottom-0 text-purple-700 opacity-25 flex justify-center items-center w-full h-full"
+			style={{
+				minWidth: "30rem",
+				minHeight: "30rem",
+				maxWidth: "60vw",
+				maxHeight: "60vw",
+			}}
+			initial={{
+				translateY: "60%",
+			}}
+			animate={{
+				rotateZ: "360deg",
+			}}
+			transition={{
+				duration: 40,
+				repeat: Infinity,
+				ease: "linear",
+			}}
+		>
+			<Star01Svg />
+		</motion.div>
+	)
 }
