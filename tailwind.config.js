07c4446b33fe867faec774e02f55bac688c840bf
zainsci <60652827+zainsci@users.noreply.github.com>
2023-04-21 14:32:31 +0300

    add darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    colors: {
-      "purple-400": "rgb(76 29 112)",
-      "purple-500": "rgb(55 33 72)",
-      "purple-600": "rgb(36 21 52)",
+	darkMode: "class",
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"purple-400": "rgb(76 29 112)",
+				"purple-500": "rgb(55 33 72)",
+				"purple-600": "rgb(36 21 52)",
 
-      teal: "rgb(39 160 155)",
-      yellow: "rgb(179 128 64)",
-    },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      },
-    },
-    fontFamily: {
-      display: ["'Lilita One'", "cursive"],
-    },
-  },
-  plugins: [],
+				"teal-2022": "rgb(39 160 155)",
+				"yellow-2022": "rgb(179 128 64)",
+			},
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+			},
+		},
+		fontFamily: {
+			display: ["'Lilita One'", "cursive"],
+		},
+	},
+	plugins: [],
 }
