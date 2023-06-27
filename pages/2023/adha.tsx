4ac68ab681766d6330b2d73c739571abe27de4bc
zainsci <60652827+zainsci@users.noreply.github.com>
2023-06-28 00:17:03 +0300

    feature: add eid-ul-adha 2023

--- pages/2023/adha.tsx
+++ pages/2023/adha.tsx
@@ -1,6 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useRouter } from "next/router"
 
 const EidAlAdha2023 = () => {
+	const router = useRouter()
+
+	useEffect(() => {
+		router.push("/")
+	})
+
 	return <div>EidAlAdha2023</div>
 }
 
